855a22d6c48ad30e57ac447b527624d40a5749c1
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-03-24 11:42:58 +0000

    update path

--- bootimg.mk
+++ bootimg.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-MKBOOTIMG := device/samsung/gta9p/mkbootimg
+MKBOOTIMG := device/samsung/gta9pwifi/mkbootimg
 
 FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
 
