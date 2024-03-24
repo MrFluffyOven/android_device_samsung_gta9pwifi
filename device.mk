763d888949068352f7fcd6633e741e93a0190a47
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-03-24 07:17:19 +0000

    initial commits

--- device.mk
+++ device.mk
@@ -6,3 +6,24 @@
 #
 
 LOCAL_PATH := device/samsung/gta9pwifi
+
+# define hardware platform
+PRODUCT_PLATFORM := holi
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock.recovery \
+    fastbootd
+
+# QCOM
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
