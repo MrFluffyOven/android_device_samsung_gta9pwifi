f83e127b33fd82f39d14eb297a16ea02227abab2
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-03-25 07:06:17 +0000

    finalise boardconfig

--- device.mk
+++ device.mk
@@ -7,23 +7,50 @@
 
 LOCAL_PATH := device/samsung/gta9pwifi
 
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
+
+# API levels
+BOARD_API_LEVEL := 30
+PRODUCT_SHIPPING_API_LEVEL := 32
+
 # define hardware platform
 PRODUCT_PLATFORM := holi
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl \
     android.hardware.boot@1.2-impl.recovery \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    bootctrl.$(PRODUCT_PLATFORM).recovery \
 
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
-    android.hardware.fastboot@1.1-impl-mock.recovery \
-    fastbootd
+    android.hardware.fastboot@1.1-impl-mock.recovery \    
+    
+    # Heath hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl
 
 # QCOM
 PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# tzdata
+PRODUCT_PACKAGES_ENG += \
+    tzdata_twrp
