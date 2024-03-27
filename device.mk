f54b824668a259ba7cf92f3f4d67db6012ec299b
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-03-27 16:07:22 +0000

    reset

--- device.mk
+++ device.mk
@@ -7,18 +7,14 @@
 
 LOCAL_PATH := device/samsung/gta9pwifi
 
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 30
-
 # API levels
-BOARD_API_LEVEL := 30
 PRODUCT_SHIPPING_API_LEVEL := 32
 
 # define hardware platform
 PRODUCT_PLATFORM := holi
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
@@ -26,7 +22,7 @@ PRODUCT_PACKAGES += \
     update_engine_sideload
 
 # Boot control HAL
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl \
     android.hardware.boot@1.2-impl.recovery \
     bootctrl.$(PRODUCT_PLATFORM) \
@@ -34,11 +30,11 @@ PRODUCT_PACKAGES += \
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
-    android.hardware.fastboot@1.1-impl-mock.recovery \    
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd   
     
     # Heath hal
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     android.hardware.health@2.1-service \
     android.hardware.health@2.1-impl
 
@@ -52,5 +48,8 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # tzdata
-PRODUCT_PACKAGES_ENG += \
+#PRODUCT_PACKAGES_ENG += \
     tzdata_twrp
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 32
