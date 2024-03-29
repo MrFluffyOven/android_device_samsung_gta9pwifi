b0cac016a8aee16cbc8c8d8d64d5bae559030c46
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-03-29 07:17:31 +0000

    update missing blobs

--- device.mk
+++ device.mk
@@ -14,29 +14,10 @@ PRODUCT_SHIPPING_API_LEVEL := 32
 PRODUCT_PLATFORM := holi
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-#PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
-
-# Boot control HAL
-#PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    bootctrl.$(PRODUCT_PLATFORM) \
-    bootctrl.$(PRODUCT_PLATFORM).recovery \
-
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock \
     fastbootd   
-    
-    # Heath hal
-#PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-service \
-    android.hardware.health@2.1-impl
 
 # Recovery modules/dependencies
 TARGET_RECOVERY_DEVICE_MODULES += \
@@ -63,10 +44,6 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
-
-# tzdata
-#PRODUCT_PACKAGES_ENG += \
-    tzdata_twrp
 
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 32
