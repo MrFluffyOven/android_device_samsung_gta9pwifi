a2da60836fb1e1f48516b5c110ed2e5c17fff3a7
Jamie Falconer <mrfluffyoven@gmail.com>
2024-03-30 09:09:05 +0000

    use twrp image builder

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -40,7 +40,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 # Initial Info
     export ALLOW_MISSING_DEPENDENCIES=true
     export LC_ALL="C"
-  
+    export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
 # Maintaining Info
     export OF_MAINTAINER=Crypton
     export FOX_VERSION="R11.1"
